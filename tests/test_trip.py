d4dc34f3ee88703173f0556213a5e2d02076f345
diagmatrix <manuelgbal1@gmail.com>
2024-04-26 13:04:32 +0200

    Comprobación y modificacion de fechas

--- tests/test_trip.py
+++ tests/test_trip.py
@@ -1,4 +1,4 @@
-from fixtures import basic_trip, trip_one_stop
+from fixtures import *
 import pytest
 
 from py_travel import Location, Trip
@@ -24,16 +24,70 @@ class TestTrip:
         except TypeError:
             pytest.fail('Invalid parameter')
 
-    def test_distance(self, basic_trip, trip_one_stop):
+    def test_distance(self, basic_trip, trip_stop):
         """
         Test that the distance calculation works
         """
         assert basic_trip.distance == 1
-        assert trip_one_stop.distance == 2
+        assert trip_stop.distance == 2
 
-    def test_distances(self, basic_trip, trip_one_stop):
+    def test_distances(self, basic_trip, trip_stop):
         """
         Test that the partial distances calculation works
         """
-        assert basic_trip.get_distances() == [1]
-        assert trip_one_stop.get_distances() == [1, 1]
+        assert basic_trip.distances == [1]
+        assert trip_stop.distances == [1, 1]
+
+    def test_travel_time(self, basic_trip, trip_stop):
+        """
+        Test that the duration calculation works
+        """
+        assert basic_trip.travel_time == 60
+        assert trip_stop.travel_time == 120
+
+    def test_travel_times(self, basic_trip, trip_stop):
+        """
+        Test that the partial duration calculation works
+        """
+        assert basic_trip.travel_times == [60]
+        assert trip_stop.travel_times == [60, 60]
+
+    @pytest.mark.parametrize(
+        ('departure', 'arrival'),
+        [
+            # No dates
+            (None, None),
+            # Only departure
+            (datetime.now(), None),
+            # Only arrival
+            (None, datetime.now()),
+            # Departure & arrival
+            (datetime.now(), datetime.now())
+        ]
+    )
+    def test_updated_dates(self, departure, arrival):
+        """
+        Test that the dates are updated after trip calculations
+        """
+
+        old_departure = departure
+        old_arrival = arrival
+        trip = Trip('', '', departure_date=departure, arrival_date=arrival)
+        trip.calculate_trip()
+
+        if not departure:
+            assert trip.departure_date != old_departure
+        if not arrival or (departure and arrival):
+            assert trip.arrival_date != old_arrival
+
+    def test_updated_dates_stops(self, trip_stop):
+        """
+        Test that the dates are updated after trip calculations for a trip with stops
+        """
+
+        stop_date = trip_stop.stops[0].departure_date
+
+        trip_stop.calculate_trip()
+
+        assert trip_stop.stops[0].departure_date != stop_date
+
