9c064aebce149ca7918e33c9bf61dceb64fc3b27
diagmatrix <manuelgbal1@gmail.com>
2024-04-29 11:52:57 +0200

    Añado wrapper para el endpoint de direcciones

--- py_travel/client.py
+++ py_travel/client.py
@@ -1,5 +1,7 @@
 import googlemaps
 
+from py_travel.exceptions import ApiError, LocationNotFoundError
+
 
 class Client:
     """
@@ -19,3 +21,22 @@ class Client:
         :param api_key: API key for Google Maps
         """
         cls.client = googlemaps.Client(key=api_key)
+
+    @classmethod
+    def directions(cls, **kwargs) -> dict:
+        """
+        Wrapper around Google Maps API to get directions
+
+        :param kwargs: googlemaps.Client directions endpoint arguments
+        :return: The response from the Google Maps API
+        """
+
+        try:
+            response = cls.client.directions(**kwargs)
+        except googlemaps.exceptions.ApiError as e:
+            if e.status == "NOT_FOUND":
+                raise LocationNotFoundError() from None
+            else:
+                raise ApiError(e.status, e.message) from None
+
+        return response
